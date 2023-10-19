e33cef2650363fca067a5ff732055e831567ae22
wh201906 <wh201906@yandex.com>
2023-10-19 22:59:55 +0800

    UART

--- app/src/main/java/io/github/wh201906/uartpipe/IOService.java
+++ app/src/main/java/io/github/wh201906/uartpipe/IOService.java
@@ -4,7 +4,10 @@ import android.app.Notification;
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.app.Service;
+import android.content.Context;
 import android.content.Intent;
+import android.hardware.usb.UsbDeviceConnection;
+import android.hardware.usb.UsbManager;
 import android.os.Binder;
 import android.os.Build;
 import android.os.IBinder;
@@ -12,6 +15,10 @@ import android.util.Log;
 
 import androidx.core.app.NotificationCompat;
 
+import com.hoho.android.usbserial.driver.UsbSerialDriver;
+import com.hoho.android.usbserial.driver.UsbSerialPort;
+
+import java.io.IOException;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
@@ -24,11 +31,14 @@ public class IOService extends Service
     private final IBinder binder = new LocalBinder();
     private Notification notification;
 
-    public int inboundPort = 18888;
+    private int inboundPort = 18888;
     private int outboundPort = 0;
     private InetAddress outboundAddress = null;
-
     private DatagramSocket udpSocket = null;
+
+    private int baudrate = 115200;
+    private UsbSerialDriver uartUsbDriver = null;
+    private UsbSerialPort uartUsbPort = null;
 
     private boolean isSocketConnected = false;
     private boolean isTrafficLoggingEnabled = false;
@@ -55,40 +65,38 @@ public class IOService extends Service
 
     public boolean startUdpSocket()
     {
-//        isServerRunning = true;
-//        serverSocket = new DatagramSocket(port); // 使用用户指定的端口
         try
         {
             udpSocket = new DatagramSocket(inboundPort);
-            isSocketConnected = true;
-        } catch(SocketException e)
+        } catch (SocketException e)
         {
             e.printStackTrace();
             return false;
         }
+        isSocketConnected = true;
 
-        new Thread(new Runnable() {
+        new Thread(new Runnable()
+        {
             @Override
-            public void run() {
-                try {
-                    while (isSocketConnected) {
+            public void run()
+            {
+                try
+                {
+                    while (isSocketConnected)
+                    {
                         byte[] receiveData = new byte[1024];
                         DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
                         udpSocket.receive(receivePacket);
 
-                        if(isTrafficLoggingEnabled)
+                        if (isTrafficLoggingEnabled)
                             Log.w(TAG, "From UDP: " + new String(receivePacket.getData(), "UTF-8"));
 
-                        DatagramPacket sendPacket = new DatagramPacket(
-                                receivePacket.getData(),
-                                receivePacket.getLength(),
-                                receivePacket.getAddress(),
-                                receivePacket.getPort()
-                        );
+                        DatagramPacket sendPacket = new DatagramPacket(receivePacket.getData(), receivePacket.getLength(), receivePacket.getAddress(), receivePacket.getPort());
 
                         udpSocket.send(sendPacket);
                     }
-                } catch (Exception e) {
+                } catch (Exception e)
+                {
                     e.printStackTrace();
                     stopUdpSocket();
                 }
@@ -98,28 +106,63 @@ public class IOService extends Service
         return true;
     }
 
-    public void stopUdpSocket() {
-        if (udpSocket != null) {
+    public void stopUdpSocket()
+    {
+        if (udpSocket != null)
+        {
             udpSocket.close();
             isSocketConnected = false;
         }
     }
+
     public void setInboundPort(int port)
     {
         inboundPort = port;
     }
+
     public void setOutboundPort(int port)
     {
         outboundPort = port;
     }
+
     public void setOutboundAddress(InetAddress address)
     {
         outboundAddress = address;
     }
+
     public void setTrafficLogging(boolean enabled)
     {
         isTrafficLoggingEnabled = enabled;
     }
+
+    public void setUartUsbDriver(UsbSerialDriver driver)
+    {
+        uartUsbDriver = driver;
+    }
+
+    public void setUartBaudrate(int baudrate)
+    {
+        this.baudrate = baudrate;
+    }
+
+    public boolean connectToUart()
+    {
+        UsbManager manager = (UsbManager) getSystemService(Context.USB_SERVICE);
+        UsbDeviceConnection connection = manager.openDevice(uartUsbDriver.getDevice());
+        if (connection == null) return false;
+        uartUsbPort = uartUsbDriver.getPorts().get(0);
+        try
+        {
+            uartUsbPort.open(connection);
+            uartUsbPort.setParameters(baudrate, 8, UsbSerialPort.STOPBITS_1, UsbSerialPort.PARITY_NONE);
+        } catch (IOException e)
+        {
+            e.printStackTrace();
+            return false;
+        }
+        return true;
+    }
+
     public class LocalBinder extends Binder
     {
         IOService getService()
@@ -127,13 +170,11 @@ public class IOService extends Service
             return IOService.this;
         }
     }
+
     private Notification createNotification()
     {
         String appName = getString(R.string.app_name);
-        NotificationCompat.Builder builder = new NotificationCompat.Builder(this, appName)
-                .setContentTitle(appName)
-                .setContentText(appName)
-                .setSmallIcon(R.mipmap.ic_launcher);
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this, appName).setContentTitle(appName).setContentText(appName).setSmallIcon(R.mipmap.ic_launcher);
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O)
         {
             NotificationChannel channel = new NotificationChannel(appName, appName, NotificationManager.IMPORTANCE_DEFAULT);
--- app/src/main/java/io/github/wh201906/uartpipe/MainActivity.java
+++ app/src/main/java/io/github/wh201906/uartpipe/MainActivity.java
@@ -1,24 +1,89 @@
 package io.github.wh201906.uartpipe;
 
+import android.app.PendingIntent;
+import android.content.BroadcastReceiver;
 import android.content.ComponentName;
+import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.content.ServiceConnection;
+import android.hardware.usb.UsbDevice;
+import android.hardware.usb.UsbManager;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.IBinder;
-import android.view.View;
+import android.util.Log;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.hoho.android.usbserial.driver.UsbSerialDriver;
+import com.hoho.android.usbserial.driver.UsbSerialProber;
+
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity
 {
+    private static final String TAG = "IOService";
+    private static final String ACTION_USB_PERMISSION = "io.github.wh201906.uartpipe.USB_PERMISSION";
+
     private IOService ioService = null;
     private boolean isIoServiceBound = false;
-    private ServiceConnection ioServiceConn = new ServiceConnection()
+
+    EditText editTextTextMultiLine;
+
+    private final BroadcastReceiver usbReceiver = new BroadcastReceiver()
+    {
+
+        public void onReceive(Context context, Intent intent)
+        {
+            String action = intent.getAction();
+            if (ACTION_USB_PERMISSION.equals(action))
+            {
+                synchronized (this)
+                {
+                    UsbDevice device = (UsbDevice) intent.getParcelableExtra(UsbManager.EXTRA_DEVICE);
+                    if (intent.getBooleanExtra(UsbManager.EXTRA_PERMISSION_GRANTED, false))
+                    {
+                        if (device != null && isIoServiceBound)
+                        {
+                            // find the driver
+                            UsbManager manager = (UsbManager) getSystemService(Context.USB_SERVICE);
+                            List<UsbSerialDriver> availableDrivers = UsbSerialProber.getDefaultProber().findAllDrivers(manager);
+                            UsbSerialDriver driver = null;
+                            for (UsbSerialDriver drv : availableDrivers)
+                            {
+                                if (drv.getDevice().equals(device))
+                                {
+                                    driver = drv;
+                                    break;
+                                }
+                            }
+                            if (driver != null)
+                            {
+                                // found corresponding driver, connect
+                                ioService.setUartUsbDriver(driver);
+                                if (ioService.connectToUart())
+                                    Toast.makeText(MainActivity.this, "UART Connected", Toast.LENGTH_SHORT).show();
+                                else
+                                    Toast.makeText(MainActivity.this, "Failed to connect to UART", Toast.LENGTH_SHORT).show();
+                            }
+
+                        }
+                    }
+                    else
+                    {
+                        Log.d(TAG, "Permission denied for device " + device);
+                    }
+                }
+            }
+        }
+    };
+    private final ServiceConnection ioServiceConn = new ServiceConnection()
     {
         @Override
         public void onServiceConnected(ComponentName className, IBinder service)
@@ -32,6 +97,9 @@ public class MainActivity extends AppCompatActivity
         public void onServiceDisconnected(ComponentName componentName)
         {
             isIoServiceBound = false;
+            Intent serviceIntent = new Intent(MainActivity.this, IOService.class);
+            if (bindService(serviceIntent, ioServiceConn, 0)) isIoServiceBound = true;
+            else Log.e(TAG, "Failed to re-bind IOService");
         }
     };
 
@@ -43,45 +111,82 @@ public class MainActivity extends AppCompatActivity
 
         Button startServerButton = findViewById(R.id.startServerButton);
         Button stopServerButton = findViewById(R.id.stopServerButton);
+        Button connectUartButton = findViewById(R.id.connectUartButton);
         TextView statusTextView = findViewById(R.id.statusTextView);
         EditText inboundPortEdit = findViewById(R.id.portEditText);
         CheckBox loggingTrafficCheckBox = findViewById(R.id.loggingTrafficCheckBox);
+        EditText baudrateEdit = findViewById(R.id.baudrateEditText);
+        editTextTextMultiLine = findViewById(R.id.editTextTextMultiLine);
 
         startServerButton.setOnClickListener(v ->
         {
-
-            if (isIoServiceBound)
+            if (!isIoServiceBound)
             {
-                ioService.setInboundPort(Integer.parseInt(inboundPortEdit.getText().toString()));
-                if (isIoServiceBound && ioService.startUdpSocket())
-                {
-                    statusTextView.setText("Server Status: Running");
-                    startServerButton.setEnabled(false);
-                    stopServerButton.setEnabled(true);
-                    inboundPortEdit.setEnabled(false);
-                }
+                return;
             }
+            ioService.setInboundPort(Integer.parseInt(inboundPortEdit.getText().toString()));
+            if (isIoServiceBound && ioService.startUdpSocket())
+            {
+                statusTextView.setText("Status: Running");
+                startServerButton.setEnabled(false);
+                stopServerButton.setEnabled(true);
+                inboundPortEdit.setEnabled(false);
+            }
+
         });
 
         stopServerButton.setOnClickListener(v ->
         {
-            if (isIoServiceBound)
+            if (!isIoServiceBound)
             {
-                ioService.stopUdpSocket();
-                stopServerButton.setEnabled(false);
-                startServerButton.setEnabled(true);
-                inboundPortEdit.setEnabled(true);
-                statusTextView.setText("Server Status: Stopped");
+                return;
             }
+            ioService.stopUdpSocket();
+            stopServerButton.setEnabled(false);
+            startServerButton.setEnabled(true);
+            inboundPortEdit.setEnabled(true);
+            statusTextView.setText("Status: Stopped");
 
+
+        });
+
+        connectUartButton.setOnClickListener(v ->
+        {
+            if (!isIoServiceBound)
+            {
+                return;
+            }
+            UsbManager manager = (UsbManager) getSystemService(Context.USB_SERVICE);
+            List<UsbSerialDriver> availableDrivers = UsbSerialProber.getDefaultProber().findAllDrivers(manager);
+            if (!availableDrivers.isEmpty())
+            {
+                ioService.setUartBaudrate(Integer.parseInt(baudrateEdit.getText().toString()));
+                UsbSerialDriver driver = availableDrivers.get(0);
+                UsbDevice device = driver.getDevice();
+                if (manager.hasPermission(device))
+                {
+                    ioService.setUartUsbDriver(driver);
+                    if (ioService.connectToUart())
+                        Toast.makeText(MainActivity.this, "UART Connected", Toast.LENGTH_SHORT).show();
+                    else
+                        Toast.makeText(MainActivity.this, "Failed to connect to UART", Toast.LENGTH_SHORT).show();
+                }
+                else
+                {
+                    PendingIntent permissionIntent = PendingIntent.getBroadcast(this, 0, new Intent(ACTION_USB_PERMISSION), PendingIntent.FLAG_IMMUTABLE);
+                    manager.requestPermission(device, permissionIntent);
+                }
+            }
         });
 
         loggingTrafficCheckBox.setOnClickListener(v ->
         {
             if (isIoServiceBound)
             {
-                ioService.setTrafficLogging(((CheckBox)v).isChecked());
+                return;
             }
+            ioService.setTrafficLogging(((CheckBox) v).isChecked());
+
         });
 
         Intent serviceIntent = new Intent(this, IOService.class);
@@ -93,8 +198,12 @@ public class MainActivity extends AppCompatActivity
         {
             startService(serviceIntent);
         }
-        if(bindService(serviceIntent, ioServiceConn, 0))
-            isIoServiceBound = true;
+        if (bindService(serviceIntent, ioServiceConn, 0)) isIoServiceBound = true;
+
+
+        IntentFilter filter = new IntentFilter(ACTION_USB_PERMISSION);
+        registerReceiver(usbReceiver, filter);
+
     }
 
     @Override
